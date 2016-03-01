805d4b59fb39d0c1bebbbd04f0ff30d92b20260c
Marco Garcia <marcogarcias.2012@gmail.com>
2016-03-01 01:29:40 -0600

    primer commit

---
+++ index.php
@@ -0,0 +1,14 @@
+﻿<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/radioNextlalpan/app/paths.php';// 'app/paths.php';
+include("php/librerias.php");
+echo inicioPagina();
+?>
+<div id="contenido" class="contenido">
+<a name="index"></a>
+<?php
+galeriaDinamica01();
+?>
+</div>
+<?php
+echo finalPagina();
+?>
